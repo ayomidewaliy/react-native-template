577f49e5070b09e12a1413cada172ac7f234a873
Waliyu Raji <mwaliyayomide@gmail.com>
2023-05-13 09:20:11 +0100

    chore: installed navigation packages and custom alias path setup

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,24 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          tests: ['./tests/'],
+          '@src': './src',
+          '@components': './src/components',
+          '@api': './src/api',
+          '@hooks': './src/hooks',
+          '@store': './src/store',
+          '@utils': './src/utils',
+          '@commons': './src/commons',
+          '@navigation': './src/navigation',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
